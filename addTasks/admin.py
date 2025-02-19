a302ad7a729a661126a30c2e948a0b1c0cd4af4a
Abdul Ahad <110593806+ahad51@users.noreply.github.com>
2025-02-19 14:09:57 +0500

    Update admin.py

--- addTasks/admin.py
+++ addTasks/admin.py
@@ -1,16 +1,1 @@
-# from django.contrib import admin
-# from .models import AddTask  # Import your model
 
-# @admin.register(AddTask)
-# class TopicsAdmin(admin.ModelAdmin):
-#     list_display = ('title', 'created_at')
-#     list_filter = ('title', 'created_at')
-#     search_fields = ('title', 'topics')
-#     readonly_fields = ('created_at', 'updated_at')
-#     def has_module_permission(self, request):
-#         # Allow access to the module only for superusers
-#         return request.user.is_superuser
-
-#     def has_view_permission(self, request, obj=None):
-#         # Allow view permissions only for superusers
-#         return request.user.is_superuser
